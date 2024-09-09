dbe36aa78bc8c005dfc5cf6ebb76bb68a2ee668f
Ana Luiza Colombi Sanfelice <155437634+anasanfelice@users.noreply.github.com>
2024-09-09 19:04:33 -0300

    Update Trabalho Intel - Ana Luiza Colombi Sanfelice.asm

--- Trabalho Intel - Ana Luiza Colombi Sanfelice.asm
+++ Trabalho Intel - Ana Luiza Colombi Sanfelice.asm
@@ -14,7 +14,7 @@ BufferTec		db 		200 dup (?)
 
 MAXSTRING		equ		200
 String			db		MAXSTRING dup (?)
-StringMaiuscula	db		MAXSTRING dup (?)
+StringMaiuscula		db		MAXSTRING dup (?)
 StringLen		db		1 dup (?)
 Linha			dw		2 dup (?)
 StrLinha		db		10 dup (?)
@@ -23,11 +23,11 @@ LinhaAtual		db		1000 dup (?)
 Flaglinha1		db		2 dup (?)
 Espacos			db		2 dup (?)
 
-SN				db		4 dup (?)
+SN			db		4 dup (?)
 BufferSN		db		4 dup (?)
 
-MsgLinha			db	"Linha ", 0
-MsgEspaco			db  ": ", 0
+MsgLinha		db	"Linha ", 0
+MsgEspaco		db  	": ", 0
 MsgPedePalavra		db	"-- Que palavra voce quer buscar?", CR, LF, 0
 MsgErroOpenFile		db	"Erro na abertura do arquivo.", CR, LF, 0
 MsgErroReadFile		db	"Erro na leitura do arquivo.", CR, LF, 0
@@ -36,66 +36,67 @@ MsgFimOcorrencias	db	"-- Fim das ocorrencias.", CR, LF, 0
 MsgNovaPalavra		db	"-- Quer buscar outra palavra? (S/N)", CR, LF, 0
 MsgNaoOcorrencias	db	"-- Nao foram encontradas ocorrencias.", CR, LF, 0
 MsgEncerrando		db	"-- Encerrando.", CR, LF, 0
-MsgPorfavor			db	"-- Por favor, somente S ou N.", CR, LF, 0
-CrLf				db 	CR, LF, 0
+MsgPorfavor		db	"-- Por favor, somente S ou N.", CR, LF, 0
+CrLf			db 	CR, LF, 0
 
-H2D		db		10 dup (?)
+H2D			db		10 dup (?)
 
-sw_n	dw	0
-sw_f	db	0
-sw_m	dw	0
+sw_n			dw	0
+sw_f			db	0
+sw_m			dw	0
 
 FlagProg			db 	0
+
 	.code
 	.startup
 	
-	push 	ds ; Salva as informacoes de segmentos
-	push 	es
-	mov 	ax, ds ; Troca DS com ES para poder usa o REP MOVSB
-	mov 	bx, es
-	mov 	ds, bx
-	mov 	es, ax
-	mov 	si, 80h ; Obtem o tamanho da linha de comando e coloca em CX
-	mov 	ch, 0
-	mov 	cl, [si]
-	mov 	ax, cx ; Salva o tamanho do string em AX, para uso futuro
-	mov 	si, 81h ; Inicializa o ponteiro de origem
-	lea 	di, CMDLINE-1 ; Inicializa o ponteiro de destino
-	rep 	movsb
-	pop 	es ; retorna os dados dos registradores de segmentos
-	pop 	ds
+	push 		ds ; Salva as informacoes de segmentos
+	push 		es
+	mov 		ax, ds ; Troca DS com ES para poder usa o REP MOVSB
+	mov 		bx, es
+	mov 		ds, bx
+	mov 		es, ax
+	mov 		si, 80h ; Obtem o tamanho da linha de comando e coloca em CX
+	mov 		ch, 0
+	mov 		cl, [si]
+	mov 		ax, cx ; Salva o tamanho do string em AX, para uso futuro
+	mov 		si, 81h ; Inicializa o ponteiro de origem
+	lea 		di, CMDLINE-1 ; Inicializa o ponteiro de destino
+	rep 		movsb
+	pop 		es ; retorna os dados dos registradores de segmentos
+	pop 		ds
 
 	
 ;====================================================================
 
 Main:
-	mov     linha, 1	; começa do 1, pois pra primeira linha ser contada
-	mov 	Flaglinha1, 1
+	mov    		linha, 1	; começa do 1, pois pra primeira linha ser contada
+	mov 		Flaglinha1, 1
 	
 	;	Solicita palavra a ser procurada
-	call	GetString
+	call		GetString
 	
 	;	 Verifica se é válida
-	lea     si, String
-    call    EhLetraOuNumero
-    cmp     ax, 1
-    jne      StringNValida
+	lea     	si, String
+    	call   		EhLetraOuNumero
+    	cmp     	ax, 1
+    	jne      	StringNValida
 
 	;	Abre arquivo
-	lea 	dx, CMDLINE
-	call 	fopen
+	lea 		dx, CMDLINE
+	call 		fopen
 	jnc		Continua1
 	
 	;	Imprime mensagem de erro abertura arquivo
 	lea		bx,MsgErroOpenFile	
-	call	printf_s
+	call		printf_s
 	
 	xor		si, si		; Indice para linha atual
 	
 Fim:	
 	lea		bx, MsgNovaPalavra
-	call	printf_s
-	call	GetSN
+	call		printf_s
+	call		GetSN
 	
 	lea		si, SN
 	mov		dl, byte ptr [si]
@@ -113,12 +114,12 @@ Fim:
 	je		RealFim
 	
 	lea		bx, MsgPorfavor
-	call	printf_s
+	call		printf_s
 	jmp		Fim
 
 RealFim:
 	lea		bx, MsgEncerrando
-	call	printf_s
+	call		printf_s
 	
 	.exit
 
@@ -136,12 +137,12 @@ Continua1:
 
 LerProximoByte:
 	mov		bx,FileHandle
-	call	getChar
+	call		getChar
 	jnc		Continua2
 	
 	;	Imprime mensagem de erro leitura arquivo
 	lea		bx,MsgErroReadFile
-	call	printf_s
+	call		printf_s
 	
 	mov		al,1
 	jmp		CloseAndRetorna
@@ -159,36 +160,36 @@ Continua2:
 	
 	
 	mov		[si], dl
-	inc 	si
+	inc 		si
 	jmp		LerProximoByte
 	
 CheckLF:
 	 ; Se for CR, verifique o próximo caractere para ver se é LF
-    mov     bx,FileHandle
-    call    getChar
-    jnc     Continua2
+    	mov     	bx,FileHandle
+    	call    	getChar
+    	jnc     	Continua2
 
-    ; Verifica se o próximo caractere é LF
-    cmp     dl, LF
-    je      FimDaLinha
+    	; Verifica se o próximo caractere é LF
+   	 cmp     	dl, LF
+    	je      	FimDaLinha
     
-    ; Se não for LF, então adiciona CR e o caractere seguinte
-    mov     [si], CR
-    inc     si
-    mov     [si], dl
-    inc     si
-    jmp     LerProximoByte
+    	; Se não for LF, então adiciona CR e o caractere seguinte
+    	mov     	[si], CR
+    	inc     	si
+    	mov     	[si], dl
+    	inc     	si
+    	jmp     	LerProximoByte
 	
 FimDaLinha:
 	mov		[si], 0	; Coloca 0 no fim da string linha atual
 	
-	call	BuscarPalavra
+	call		BuscarPalavra
 	
 	mov		ax, Linha	; Linha++
 	inc		ax
 	mov		Linha, ax
 	
-	call	LimparLinhaAtual
+	call		LimparLinhaAtual
 	
 	xor		si, si ;Inicio proxima palavra
 	lea		si, LinhaAtual	;Inicio linha
@@ -199,19 +200,19 @@ CloseAndRetorna:
 	cmp		Flaglinha1, 0
 	jne		NOcorrencias
 	
-	lea 	bx, MsgFimOcorrencias
-	call	printf_s
-	jmp 	FechaArquivo
+	lea 		bx, MsgFimOcorrencias
+	call		printf_s
+	jmp 		FechaArquivo
 	
 NOcorrencias:
 	lea		bx, MsgNaoOcorrencias
-	call	printf_s
+	call		printf_s
 
 FechaArquivo:	
-    mov     bx,FileHandle
-    call    fclose
+   	mov     	bx,FileHandle
+    	call    	fclose
 
-    jmp Fim
+    	jmp 		Fim
 	
 ;=======================================================================
 ; *********FUNÇÕES***************
@@ -221,96 +222,97 @@ FechaArquivo:
 ; Função: Verifica se a string contém apenas letras e números
 ;--------------------------------------------------------------------
 EhLetraOuNumero proc near
-    mov     al, [si]          ; Carrega o primeiro caractere da string
+    	mov     	al, [si]          ; Carrega o primeiro caractere da string
 
 VerificarCaractere:
-    cmp     al, 0             
-    je      EhLetraOuNumeroFim
-    ; Verifica se o caractere é uma letra maiúscula
-    cmp     al, 'A'           
-    jb      CaractereInvalido
-    cmp     al, 'Z'
-    jbe     ProximoCaractere 
+    	cmp     	al, 0             
+    	je      	EhLetraOuNumeroFim
+
+    	; Verifica se o caractere é uma letra maiúscula
+    	cmp     	al, 'A'           
+    	jb      	CaractereInvalido
+    	cmp     	al, 'Z'
+    	jbe     	ProximoCaractere 
     
-    ; Verifica se o caractere é uma letra minúscula
-    cmp     al, 'a'
-    jb      CaractereInvalido
-    cmp     al, 'z'
-    jbe     ProximoCaractere
+    	; Verifica se o caractere é uma letra minúscula
+    	cmp     	al, 'a'
+    	jb      	CaractereInvalido
+    	cmp     	al, 'z'
+    	jbe     	ProximoCaractere
     
-    ; Verifica se o caractere é um número
-    cmp     al, '0'
-    jb      CaractereInvalido
-    cmp     al, '9'
-    jbe     ProximoCaractere
+    	; Verifica se o caractere é um número
+    	cmp     	al, '0'
+   	jb      	CaractereInvalido
+    	cmp     	al, '9'
+    	jbe     	ProximoCaractere
 	
    
 CaractereInvalido:
-    xor     ax, ax             ; Define AX como 0 (não é uma string válida)
-    jmp     FimProc
+    	xor     	ax, ax             ; Define AX como 0 (não é uma string válida)
+    	jmp     	FimProc
 
 ProximoCaractere:
-    inc     si                 ; Avança para o próximo caractere na string
-    mov     al, [si]           
-    jmp     VerificarCaractere
+    	inc     	si                 ; Avança para o próximo caractere na string
+    	mov     	al, [si]           
+    	jmp     	VerificarCaractere
 
 EhLetraOuNumeroFim:
-    mov     ax, 1              ; Define AX como 1 (string válida)
+   	 mov     	ax, 1              ; Define AX como 1 (string válida)
 
 FimProc:
-    ret
+    	ret
 EhLetraOuNumero endp
 
 ;--------------------------------------------------------------------
 ;	Buscar palavra
 ;-----------------------------------------------------------------
 BuscarPalavra proc
-    lea 	si, LinhaAtual      ; SI aponta para o início da linha
-    lea		di, String          ; DI aponta para o início da palavra buscada
+    	lea 		si, LinhaAtual      ; SI aponta para o início da linha
+    	lea		di, String          ; DI aponta para o início da palavra buscada
 
 ProximaPalavra:
-    push 	si                 ; Salva o ponteiro de linha atual
+    	push	 	si                 ; Salva o ponteiro de linha atual
 
 CompararCaracter:
-	mov 	dl, byte ptr [si]            ; Carrega o próximo caractere da linha
+	mov 		dl, byte ptr [si]            ; Carrega o próximo caractere da linha
 	mov		dh, byte ptr [di]
  
 	mov		al, dl
-	call	Maiuscula
-	mov 	dl, al
+	call		Maiuscula
+	mov 		dl, al
 	
 	mov		al, dh
-	call	Maiuscula
+	call		Maiuscula
 	mov		dh, al
 	
 ContinueChecagem:
-	cmp 	dh, 0    ; Verifica o fim da palavra buscada
-    je  	PalavraEncontrada
+	cmp 		dh, 0    ; Verifica o fim da palavra buscada
+    	je  		PalavraEncontrada
 	
-    cmp 	dl, dh             ; Compara os caracteres
-    jne 	ContinuarBusca      ; Se não coincidir, continue a busca
+    	cmp 		dl, dh             ; Compara os caracteres
+    	jne 		ContinuarBusca      ; Se não coincidir, continue a busca
 	
-    inc 	si
-    inc 	di
-    jmp 	CompararCaracter
+    	inc 		si
+    	inc 		di
+    	jmp 		CompararCaracter
 
 PalavraEncontrada:
-    call 	ImprimirLinha      ; Imprime a linha se a palavra for encontrada
-    pop 	si                 
-    lea 	di, String          
-    inc 	si                  ; Continua buscando a partir do próximo caractere
-    jmp 	ProximaPalavra      ; Continua buscando na linha atual
+    	call 		ImprimirLinha      ; Imprime a linha se a palavra for encontrada
+    	pop 		si                 
+    	lea 		di, String          
+    	inc 		si                  ; Continua buscando a partir do próximo caractere
+    	jmp 		ProximaPalavra      ; Continua buscando na linha atual
 
 ContinuarBusca:
-    pop 	si                  
-    lea 	di, String          
-    inc 	si                  ; Avança para o próximo caractere na linha
-    cmp 	byte ptr [si], 0    ; Verifica se o final da linha foi alcançado
-    je  	Retornar
-    jmp 	ProximaPalavra      ; Continua a buscar a próxima palavra
+    	pop 		si                  
+   	lea 		di, String          
+    	inc 		si                  ; Avança para o próximo caractere na linha
+    	cmp 		byte ptr [si], 0    ; Verifica se o final da linha foi alcançado
+    	je  		Retornar
+    	jmp 		ProximaPalavra      ; Continua a buscar a próxima palavra
 
 Retornar:
-    ret
+    	ret
 BuscarPalavra endp
 
 ;-------------------------------------------------------------------
@@ -324,22 +326,22 @@ ImprimirLinha proc near
 	
 	mov		Flaglinha1, 0
 	lea		bx, MsgOcorrencias
-	call	printf_s
+	call		printf_s
 	
 printlinha:
-	lea 	bx, MsgLinha
-	call	printf_s
+	lea 		bx, MsgLinha
+	call		printf_s
 	
-	call 	printf_linha
+	call 		printf_linha
 	
 	lea		bx, MsgEspaco
-	call 	printf_s
+	call 		printf_s
 	
 	lea		bx, LinhaAtual
-	call	printf_S
+	call		printf_S
 	
 	lea		bx, CrLF
-	call	printf_s
+	call		printf_s
 	ret
 ImprimirLinha endp
 
@@ -360,15 +362,15 @@ Maiuscula endp
 ; Função: Limpar a linha atual
 ;--------------------------------------------------------------------
 LimparLinhaAtual proc near
-    lea     di, LinhaAtual  ; Aponta para o início da LinhaAtual
-    mov     cx, MAXSTRING   
+    	lea     	di, LinhaAtual  ; Aponta para o início da LinhaAtual
+    	mov     	cx, MAXSTRING   
 
 limpa_loop:
-    mov     byte ptr [di], 0 
-    inc     di               ; Avança para o próximo byte
-    loop    limpa_loop       ; Repete até que cx seja zero
+    	mov     	byte ptr [di], 0 
+    	inc    		di               ; Avança para o próximo byte
+    	loop    	limpa_loop       ; Repete até que cx seja zero
 
-    ret
+    	ret
 LimparLinhaAtual endp
 
 ;-------------------------------------------------------------------
@@ -377,10 +379,10 @@ LimparLinhaAtual endp
 printf_linha	proc near
 	mov		ax, Linha
 	lea		bx,StrLinha
-	call	sprintf_w
+	call		sprintf_w
 	
 	lea		bx, StrLinha
-	call	printf_s
+	call		printf_s
 	ret
 printf_linha endp
 
@@ -437,7 +439,7 @@ GetSN	proc	near
 	int		21h
 	
 	lea		bx, CrLF
-	call	printf_s
+	call		printf_s
 
 	;	// Copia do buffer de teclado para o a string
 	lea		si,BufferSN+2
@@ -446,7 +448,7 @@ GetSN	proc	near
 	mov		ch,0
 	mov		ax,ds						; Ajusta ES=DS para poder usar o MOVSB
 	mov		es,ax
-	rep 	movsb
+	rep 		movsb
 
 	;	// Coloca o '\0' no final do string
 	;	*d = '\0';
@@ -461,7 +463,7 @@ GetSN	endp
 GetString	proc	near
 
 	lea		bx,MsgPedePalavra
-	call	printf_s
+	call		printf_s
 
 	mov		ah,0ch
 	lea		dx,BufferTec
@@ -469,7 +471,7 @@ GetString	proc	near
 	int		21h
 	
 	lea		bx, CrLF
-	call	printf_s
+	call		printf_s
 
 	;	// Copia do buffer de teclado para o a string
 	lea		si,BufferTec+2
@@ -572,7 +574,7 @@ printf_s	proc	near
 	cmp		dl,0
 	je		ps_1
 
-	push	bx
+	push		bx
 	mov		ah,2
 	int		21H
 	pop		bx
